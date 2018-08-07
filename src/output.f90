e5d5c4d1ab8998dace5a89563a6a53aee54d0d37
Duncan Forgan <dhf3@st-andrews.ac.uk>
2018-08-07 15:11:45 +0100

    Added a flush statement for log files

--- src/output.f90
+++ src/output.f90
@@ -31,10 +31,10 @@ write(isnap,102) mass(ibody), pos(:,ibody), vel(:,ibody), acc(:,ibody),&
    
 else
 
-   ! or 
- output individual bodies to separate files
+   ! or output individual bodies to separate files
 
    do ibody=1,N
+        print*, ibody, ibody+ilog, t/twopi, mass(ibody)
       write(ibody+ilog,102) t/twopi, mass(ibody),pos(:,ibody), vel(:,ibody), acc(:,ibody),&
 semimaj(ibody),ecc(ibody),inc(ibody),longascend(ibody),argper(ibody),trueanom(ibody), &
 ekin(ibody),epot(ibody),etot(ibody),angmom(:,ibody)
@@ -46,6 +46,6 @@ endif
 ! Write log file containing global simulation data
 
 write(ilog,103) t/twopi, dt/twopi, 100.0*maxerror/tolerance, system_energy, dE, system_ang, dL
-
+call flush(ilog)
 
 end subroutine output
